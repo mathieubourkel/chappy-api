b9e3e9c17e326b4493365d5ef9f0f90042d3a970
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-02-14 16:52:56 +0100

    Nats

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import {
+  UberModule
+} from '@app/uber/uber.module';
 
 @Module({
+  imports: [UberModule],
   controllers: [CommentController],
-  providers: [CommentService],
 })
 export class CommentModule {}
